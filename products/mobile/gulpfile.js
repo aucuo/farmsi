a362d8b44f3f87c0620406762782d75b1a7befd8
aucuo <jahor.sykaviec@gmail.com>
2024-03-04 15:55:30 +0300

    styles reorganization and some optimizations

--- products/mobile/gulpfile.js
+++ products/mobile/gulpfile.js
@@ -1,17 +1,17 @@
 const { src, dest, watch, series } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
-const sourcemaps = require('gulp-sourcemaps'); // Add this line
+const sourcemaps = require('gulp-sourcemaps');
 
 function buildStyles() {
-    return src('./css/components/pages/products.scss')
-        .pipe(sourcemaps.init()) // Initialize sourcemaps before compiling SASS
+    return src('./sass/**/*.scss')
+        .pipe(sourcemaps.init())
         .pipe(sass().on('error', sass.logError))
-        .pipe(sourcemaps.write()) // Write sourcemaps after compiling SASS
-        .pipe(dest('./css'));
+        .pipe(sourcemaps.write())
+        .pipe(dest('./css/')) // Сохранить в папку ./css/components
 }
 
 function watchTask() {
-    watch(['./css/**/*.scss'], buildStyles);
+    watch(['./sass/**/*.scss'], buildStyles); // Отслеживать все файлы .scss
 }
 
 exports.default = series(buildStyles, watchTask);
