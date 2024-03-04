dbe077b7a16a6d10b8af9c93cb8575fce93ec25b
aucuo <jahor.sykaviec@gmail.com>
2024-03-04 14:43:41 +0300

    issues fix, storage optimization & reogragization

--- mobile/products/mobile_template/article/script.js
+++
@@ -1,5 +0,0 @@
-var articleSlider = new swiper__WEBPACK_IMPORTED_MODULE_0__["default"](".article-slider .swiper", {
-    loop: !1,
-    slidesPerView: 'auto',
-    spaceBetween: 8,
-});
--- mobile/products/mobile_template/pharmacy/script.js
+++
@@ -1,9 +0,0 @@
-__webpack_require__.r(__webpack_exports__);
-/* harmony import */ var swiper__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! swiper */ "./node_modules/swiper/swiper.esm.js");
-/* eslint-disable no-new */
-
-var pharmacySlider = new swiper__WEBPACK_IMPORTED_MODULE_0__["default"](".pharmacy-slider .swiper", {
-    loop: false,
-    slidesPerView:'auto',
-    spaceBetween: 8,
-});
