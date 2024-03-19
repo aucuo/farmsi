1d7f55d630b3a517b98d8214fe8c27a7df8160ea
aucuo <jahor.sykaviec@gmail.com>
2024-03-19 19:08:39 +0300

    customers mobile edits p.2

--- products/mobile/js/main.js
+++ products/mobile/js/main.js
@@ -618,3 +618,91 @@ $(document).ready(function () {
         }
     });
 });
+
+var checkSegmentedControl = function checkSegmentedControl() {
+    if (this.checked) {
+        var parent = $(this).closest('.segmented-control');
+        var thumb = parent.find('.segmented-control__color');
+
+        // Действие при выборе "Фото"
+        if (this.value == 'photos') {
+            var width = parent.find('label[for="tab-1"]').width() + 24; // width + padding
+            thumb.stop(true, true).animate({
+                left: 2,
+                width: width,
+            }, 300);
+            thumb.css('right', 'unset');
+
+            $('button[data-fancybox-close]').click();
+            setTimeout(function () {
+                $('a[data-fancybox="product"] img')[0].click();
+            }, 10);
+        }
+        // Действие при выборе "Сертификаты"
+        else if (this.value == 'certs') {
+            var width = parent.find('label[for="tab-2"]').width() + 24; // width + padding
+            thumb.css('left', 'unset');
+            thumb.stop(true, true).animate({
+                right: 2,
+                width: width,
+            }, 300);
+
+            $('button[data-fancybox-close]').click();
+            setTimeout(function () {
+                $('a[data-fancybox="cert"] img')[0].click();
+            }, 10);
+        }
+    }
+}
+
+var showSegmentedControl = function showSegmentedControl(fancybox) {
+    var trigger = $(fancybox.options.triggerEl);
+    var type = trigger.data('fancybox')
+
+    if (type == "product" || type == "cert") {
+        var segmentedControl = $('.js-segmented');
+
+        segmentedControl.fadeIn(100);
+    }
+}
+
+var closeSegmentedControl = function closeSegmentedControl(fancybox) {
+    var segmentedControl = $('.js-segmented');
+    segmentedControl.fadeOut(100);
+}
+
+$(document).ready(function () {
+    $('input[name="segmented-control"][checked]').each(checkSegmentedControl);
+    $('input[type="radio"][name="segmented-control"]').change(checkSegmentedControl);
+});
+
+var fancybox = Fancybox.bind("[data-fancybox]", {
+    Toolbar: {
+        display: {
+            left: ["infobar"],
+            right: ["close"],
+        },
+    },
+    Thumbs: {
+        type: "classic",
+    },
+    contentClick: "iterateZoom",
+    Images: {
+        initialSize: "fit",
+        Panzoom: {
+            maxScale: 2,
+        },
+    },
+    Carousel: {
+        Navigation: false,
+    },
+    on: {
+        init: (fancybox, slide) => {
+            showSegmentedControl(fancybox);
+        },
+        close: (fancybox, slide) => {
+            closeSegmentedControl(fancybox);
+        },
+    },
+    touch: false
+});
