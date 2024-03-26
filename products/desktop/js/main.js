32569ce2ce5671dbb32ea0d2a9f0b01ad6811403
aucuo <jahor.sykaviec@gmail.com>
2024-03-26 04:05:09 +0300

    change location desktop v.1

--- products/desktop/js/main.js
+++ products/desktop/js/main.js
@@ -1996,3 +1996,39 @@ function init() {
         features: placemarks
     });
 }
+
+// CITY CHOOSE
+
+$(document).ready(function () {
+    var cityPopup = $('.city-popup');
+    cityPopup.fadeIn(300);
+    const cityChangeModal = new bootstrap.Modal($('#cityChangeModal'), {});
+    cityChangeModal.show();
+    var fixedCityPopup = function fixedCityPopup() {
+        var scroll = $(window).scrollTop();
+        var cityPopup = $('.city-popup');
+        var header = $(".header__top");
+        var headerHeight = header.innerHeight();
+
+        if (scroll >= headerHeight) {
+            cityPopup.addClass("is-active");
+        } else {
+            cityPopup.removeClass("is-active");
+        }
+    };
+    fixedCityPopup();
+    $(window).scroll(fixedCityPopup);
+});
+
+function submitLocation() {
+    var cityPopup = $('.city-popup');
+    // cityPopup.fadeOut(300);
+}
+
+function changeLocation() {
+    var cityPopup = $('.city-popup');
+    // cityPopup.fadeOut(300);
+
+    const cityChangeModal = new bootstrap.Modal($('#cityChangeModal'), {});
+    cityChangeModal.show();
+}
